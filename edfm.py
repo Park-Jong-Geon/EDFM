a8e5e5ef464b6e417d4442c890b58e192fe5b4ec
Park-Jong-Geon <panyaang99@gmail.com>
2025-08-16 00:01:50 +0900

    bug fixed

--- edfm.py
+++ edfm.py
@@ -358,11 +358,9 @@ def launch(config):
 
     # Label with teacher predictions
     @partial(jax.pmap, axis_name="batch")
-    def step_label(state, batch):
+    def step_label(state, batch, swag_state):
         rng = state.rng
-        swag_state = random.choice(swag_state_list)
         swag_param = sample_swag_diag(1, rng, swag_state)[0]
-        rng, _ = jax.random.split(rng)
         res_params_dict = dict(params=swag_param)
         
         images = batch["images"]
@@ -580,7 +578,11 @@ def launch(config):
             batch["images"] /= 255.0
             if config.mixup_alpha > 0:
                 batch = step_mixup(state, batch)
-            batch = step_label(state, batch)
+                
+            swag_state = random.choice(swag_state_list)
+            swag_state = jax_utils.replicate(swag_state)    
+            batch = step_label(state, batch, swag_state)
+            
             state, metrics = step_train(state, batch)
             train_metrics.append(metrics)
 
